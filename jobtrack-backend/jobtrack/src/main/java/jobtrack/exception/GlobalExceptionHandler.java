8ad25059854c7b0a1efce50fab8d3c9eebd385f7
Aniket Phand <aniketphand21@gmail.com>
2026-03-27 22:07:53 +0530

    Improved validation error handling with custom response

--- jobtrack-backend/jobtrack/src/main/java/jobtrack/exception/GlobalExceptionHandler.java
+++ jobtrack-backend/jobtrack/src/main/java/jobtrack/exception/GlobalExceptionHandler.java
@@ -1,15 +1,33 @@
 package jobtrack.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Handle validation errors
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return errors;
+    }
+
+    // Handle runtime errors
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleRuntimeException(RuntimeException ex) {
         return ex.getMessage();
     }
 }
-
