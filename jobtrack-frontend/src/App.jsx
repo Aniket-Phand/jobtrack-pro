10748b0e4b6eb7ee6943415b7867c6e7c6a6f037
Aniket-Phand <aniketphand13@gmail.com>
2026-03-29 18:33:10 +0530

    Jobtrack Frontend initail setup

---
+++ jobtrack-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <h1>JobTrack Pro 🚀</h1>
+      <p>Testing Frontend Setup</p>
+    </div>
+  );
+}
+
+export default App
